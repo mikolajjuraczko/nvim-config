bc52cc98998fe88fcdabfeeec5eab56843b695d9
Mikolaj Juraczko <mikolaj@mikolajjuraczko.com>
2022-11-11 21:13:28 +0100

    Changed some nvim-tree mappings

--- lua/nvimtree.lua
+++ lua/nvimtree.lua
@@ -1,3 +1,11 @@
 require("nvim-tree").setup({
+	view = {
+		mappings = {
+			list = {
+				{ key = "d", action = "trash" },
+				{ key = "D", action = "remove" },
+			},
+		},
+	},
 	renderer = { special_files = {} },
 })
